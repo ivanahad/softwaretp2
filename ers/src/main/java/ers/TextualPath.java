1d739e6cbe5c5bd529618eaaffb00f34ab6b8bfa
edd19 <eddyndizera@gmail.com>
2016-10-25 18:42:09 +0200

    show textual path

--- ers/src/main/java/ers/TextualPath.java
+++ ers/src/main/java/ers/TextualPath.java
@@ -8,4 +8,26 @@ public class TextualPath extends Path{
 		this.language=language;
 	}
 
+	public String getPath(){
+		String path = "Go in direction of ";
+		if (start.coordinate.getY() < end.coordinate.getY()){ // North
+			path = path + "N";
+		}
+		else if (start.coordinate.getY() > end.coordinate.getY()){ //South
+			path = path + "S";
+		}
+		if (start.coordinate.getX() < end.coordinate.getX()){ // East
+			path = path + "E";
+		}
+		else if (start.coordinate.getX() > end.coordinate.getX()){ // West
+			path = path + "W";
+		}
+		if (start.coordinate.getY() == end.coordinate.getY() &&
+				start.coordinate.getX() == end.coordinate.getX()){
+			path = "You've arrived";
+		}
+
+		return path;
+	}
+
 }
