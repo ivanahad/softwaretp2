f1f7d18cd95acf3ef82531763be665f5e255403e
edd19 <eddy.ndizera@student.uclouvain.be>
2016-11-23 09:57:41 +0100

    Added radius to class location

--- ers/src/main/java/ers/Location.java
+++ ers/src/main/java/ers/Location.java
@@ -3,11 +3,18 @@ package ers;
 
 public class Location {
 	public Coordinate coordinate;
+	public int radius;
 	
 	public Location (Coordinate coordinate) {
 		this.coordinate = coordinate;
+		this.radius = 0;
 	}
-	
+
+	public Location(Coordinate coordinate, int radius) {
+		this.coordinate = coordinate;
+		this.radius = radius;
+	}
+
 	public void updateLocation(Coordinate coordinate){
 		this.coordinate=coordinate;
 	}
@@ -17,6 +24,6 @@ public class Location {
 		double longueur = otherLocation.coordinate.getX()-this.coordinate.getX();
 		double largeur = otherLocation.coordinate.getY()-this.coordinate.getY();
 		double distance = Math.sqrt(Math.pow(longueur, 2) + Math.pow(largeur, 2));
-		return distance;
+		return distance - this.radius - otherLocation.radius;
 	}
 }
--- ers/src/test/java/ers/testLocation.java
+++ ers/src/test/java/ers/testLocation.java
@@ -8,12 +8,22 @@ public class testLocation {
 	
 	
 	@Test
-	public void distanceLocations(){
+	public void distanceLocationsWithoutRadius(){
 		Coordinate coordinate = new Coordinate(0,0);
 		Location location = new Location(coordinate);
 		
 		Coordinate coordinateOther = new Coordinate(3,4);
 		Location otherLocation = new Location(coordinateOther);
 		assertEquals(location.distanceLocations(otherLocation), 5.0, 0.001);	
+	}
+
+	@Test
+	public void distanceLocationsWithRadius(){
+		Coordinate coordinate = new Coordinate(0,0);
+		Location location = new Location(coordinate, 1);
+
+		Coordinate coordinateOther = new Coordinate(3,4);
+		Location otherLocation = new Location(coordinateOther, 1);
+		assertEquals(location.distanceLocations(otherLocation), 3.0, 0.001);
 	}
 }
